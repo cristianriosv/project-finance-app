75603fd1e4c08e7008282753a0b0d04403a9caa7
Cristian Rios <cristianriosv@gmail.com>
2023-06-18 14:05:02 +0200

    fix: allow min width to input components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@ import { ThemeProvider } from "@material-tailwind/react";
 import ProjectsView from "./views/ProjectsView/ProjectsView";
 import './styles/index.css';
 
+const customTheme = {
+  input: {
+      styles: {
+          base: {
+              container: {
+                  minWidth: 'min-w-[20px]'
+              }
+          }
+      }
+  },
+  select: {
+	styles: {
+		base: {
+			container: {
+				minWidth: 'min-w-[20px]'
+			}
+		}
+	}
+  }
+}
+
 function App() {
-  return (
-    <ThemeProvider>
-      <ProjectsView />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider value={customTheme}>
+			<ProjectsView />
+		</ThemeProvider>
+	);
 }
 
 export default App;
